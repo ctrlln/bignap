128f7da2d3e4747e789bc1a64486fb26d064a9ec
oliver.kurzaj <oliver.kurzaj@gmail.com>
2025-12-26 23:13:29 +0100

    fixed data link

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 import type { Database } from '../types';
 
 export async function fetchData(): Promise<Database> {
-    const response = await fetch('/data/db.json');
+    const response = await fetch(`${import.meta.env.BASE_URL}data/db.json`);
     if (!response.ok) {
         throw new Error('Failed to fetch data');
     }
